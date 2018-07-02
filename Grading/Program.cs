59fdad1357e18605b1d70c54314e719664fa06af
Esat001 <Esat001@ESAT001-PC>
2018-07-02 10:37:14 +0300

    Grading

--- Grading/Program.cs
+++ Grading/Program.cs
@@ -6,7 +6,31 @@ namespace Grading
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please Enter Marks to get your Grade:");
+            int Marks = Int16.Parse(Console.ReadLine());
+            {
+                if (Marks <= 100) { }
+            }
+            Console.WriteLine("Grade A");
+            {
+                if (Marks <= 80) { }
+            }
+            Console.WriteLine("Grade B");
+            {
+                if (Marks <= 60) { }
+            }
+            Console.WriteLine("Grade C");
+            {
+                if (Marks <= 50) { }
+            }
+            Console.WriteLine("Grade D");
+            {
+
+   
+                 else if (Marks <= 40) { }
+            }
+            Console.WriteLine("FAIL");
+ 
         }
     }
 }
